a3279a72746663efae8a21eb06886e061811f67b
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2020-08-26 18:37:19 +0900

    no message

--- App.js
+++ App.js
@@ -16,11 +16,17 @@ export default class extends React.Component {
     const {
       data: {
         main: { temp },
+        weather,
       },
     } = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     );
-    this.setState({ isLoading: false, temp });
+    this.setState({
+      isLoading: false,
+      temp,
+      condition: weather[0].main,
+    });
+    console.log(this.state);
   };
 
   getLocation = async () => {
@@ -40,7 +46,11 @@ export default class extends React.Component {
   }
 
   render() {
-    const { isLoading, temp } = this.state;
-    return isLoading ? <Loading /> : <Weather temp={temp} />;
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
   }
 }
--- Weather.js
+++ Weather.js
@@ -1,18 +1,89 @@
 import React from "react";
 import { StyleSheet, View, Text, StatusBar } from "react-native";
 import PropTypes from "prop-types";
-import { Fontisto } from "@expo/vector-icons";
+import { Fontisto, Feather } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 
-export default function Weather({ temp }) {
+const weatherOption = {
+  Thunderstorm: {
+    icon: "day-lightning",
+    gradient: ["#373B44", "#F3F9A7"],
+  },
+  Drizzle: {
+    icon: "rain",
+    gradient: ["#0083B0", "#00B4DB"],
+  },
+  Rain: {
+    icon: "rains",
+    gradient: ["#000046", "#1CB5E0"],
+  },
+  Snow: {
+    icon: "snow",
+    gradient: ["#C9D6FF", "#E2E2E2"],
+  },
+  Atmosphere: {
+    icon: "cloudy-gusts",
+    gradient: ["", ""],
+  },
+  Mist: {
+    icon: "cloudy-gusts",
+    gradient: ["#8e9eab", "#eef2f3"],
+  },
+  Smoke: {
+    icon: "cloudy-gusts",
+    gradient: ["8e9eab", "#eef2f3"],
+  },
+  Haze: {
+    icon: "cloudy-gusts",
+    gradient: ["8e9eab", "#eef2f3"],
+  },
+  Dust: {
+    icon: "cloudy-gusts",
+    gradient: ["8e9eab", "#eef2f3"],
+  },
+  Fog: {
+    icon: "cloudy-gusts",
+    gradient: ["8e9eab", "#eef2f3"],
+  },
+  Sand: {
+    icon: "cloudy-gusts",
+    gradient: ["#3E5151", "#DECBA4"],
+  },
+  Ash: {
+    icon: "cloudy-gusts",
+    gradient: ["#3E5151", "#DECBA4"],
+  },
+  Squall: {
+    icon: "cloudy-gusts",
+    gradient: ["#73C8A9", "#373B44"],
+  },
+  Tornado: {
+    icon: "cloudy-gusts",
+    gradient: ["#00467F", "#A5CC82"],
+  },
+  Clouds: {
+    icon: "cloudy",
+    gradient: ["#bdc3c7", "#2c3e50"],
+  },
+  Clear: {
+    icon: "horizon-alt",
+    gradient: ["#2980B9", "#6DD5FA"],
+  },
+};
+
+export default function Weather({ temp, condition }) {
   return (
     <LinearGradient
-      colors={["#4c669f", "#3b5998", "#192f6a"]}
+      colors={weatherOption[condition].gradient}
       style={styles.container}
     >
       <StatusBar barStyle="light-content"></StatusBar>
       <View style={styles.halfContainer}>
-        <Fontisto size={100} name="rain" color="white" />
+        <Fontisto
+          size={100}
+          name={weatherOption[condition].icon}
+          color="white"
+        />
         <Text style={styles.temp}>{temp}</Text>
       </View>
       <View style={styles.halfContainer}></View>
@@ -23,13 +94,23 @@ export default function Weather({ temp }) {
 Weather.prototype = {
   temp: PropTypes.number.isRequired,
   condition: PropTypes.oneOf([
+    "Clear",
     "Thunderstorm",
     "Drizzle",
     "Rain",
     "Snow",
     "Atmosphere",
-    "Clear",
     "Clouds",
+    "Smoke",
+    "Mist",
+    "Smoke",
+    "Haze",
+    "Dust",
+    "Fog",
+    "Sand",
+    "Ash",
+    "Squall",
+    "Tornado",
   ]).isRequired,
 };
 
